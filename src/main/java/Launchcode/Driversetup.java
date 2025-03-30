90d2932fd303499362a11b7a751efc1bdad13254
Aakas99 <spaakash2608@gmail.com>
2025-03-30 19:01:33 +0530

    Driver setup update

--- src/main/java/Launchcode/Driversetup.java
+++ src/main/java/Launchcode/Driversetup.java
@@ -1,5 +1,35 @@
 package Launchcode;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Driversetup {
 
+	public WebDriver driver;
+	
+	@Test
+	@Parameters({"url", "browser"})
+	public void driverstart(String url, String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver= new ChromeDriver();
+			
+		}
+		else if(browser.equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver= new EdgeDriver();
+			
+		}
+		
+		driver.get(url);
+		driver.manage().window().maximize();
+		System.out.println("Page Title " + driver.getTitle() );
+	}
+	
+	
 }
