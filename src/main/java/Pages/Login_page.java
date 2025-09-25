d83f12a2a17731d339e53424eacbb484d3b5c052
Aakas99 <spaakash2608@gmail.com>
2025-09-25 19:29:09 +0530

    Screenshot integrated

--- src/main/java/Pages/Login_page.java
+++ src/main/java/Pages/Login_page.java
@@ -3,13 +3,16 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import CustomCode.Screenshot;
 import CustomCode.textfilereader;
-import Launchcode.Driversetup;
+
 
 
 public class Login_page  {
 	textfilereader text = new textfilereader();
 	WebDriver driver;
+	Screenshot sc = new Screenshot();
 	
 	public Login_page(WebDriver driversetup) {
 		this.driver= driversetup;
@@ -17,17 +20,20 @@ public class Login_page  {
 	
 	public WebElement username() {
 		WebElement UserName = driver.findElement(By.xpath(text.textreader("Username")));
+		sc.captureElementScreenshot(UserName, "Username field");
 		return UserName;
 		
 	}
 	public WebElement Password() {
 		WebElement Password = driver.findElement(By.xpath(text.textreader("Password")));
+		sc.captureElementScreenshot(Password, "Password field");
 		return Password;
 		
 		
 	}
 	public WebElement LoginBT() {
 		WebElement LoginBT = driver.findElement(By.xpath(text.textreader("LoginBT")));
+		sc.captureElementScreenshot(LoginBT, "Login Button field");
 		return LoginBT;
 		
 	}
