7ad9a1f4f8cd268733df99a49ca6a64bd1db7758
Aakas99 <spaakash2608@gmail.com>
2025-09-25 19:13:40 +0530

    Screenshot code update

--- src/main/java/CustomCode/Practice.java
+++ src/main/java/CustomCode/Practice.java
@@ -1,35 +1,49 @@
 package CustomCode;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+
+import java.util.List;
+
 
 public class Practice {
+	
 	public static void main(String[] args){
-		String a = "34test909gda@sdet!23pro";
-	      char[] b = a.toCharArray();
-	      int left =0;
-	      int right = b.length-1;
-	      
-	      while(left<right) {
-	    	  if(!Character.isLetter(b[left])) {
-	    		  left++;
-	    	  }
-	    	  else if(!Character.isLetter(b[right])) {
-	    		  right--;
-	    	  }
-	    	  else {
-	    		  char temp = b[left];
-		    	  b[left]= b[right];
-		    	  b[right]= temp;
-		    	  left++;
-		    	  right--;
-	    	  }
-	    	  
-	    	  
-	    	  
-	    	  
-	      }
-	      String result = new String(b);
-	      System.out.println(result);
+		 String a = "BOBERRE";
+	        String longestPalindrome = ""; // Variable to store the longest palindrome found
+	        List<String> allPalindromes = new ArrayList<>(); // To store all found palindromes
+	        
+	        // --- 1. Robust Palindrome Detection ---
+	        // 'i' is the starting index, 'j' is the ending index (non-inclusive for substring)
+	        for(int i = 0; i < a.length(); i++) {
+	            // Start j from i to include single-character substrings
+	            for(int j = i + 1; j <= a.length(); j++) { 
+	                
+	                String substring = a.substring(i, j);
+	                
+	                // Check if the substring is a palindrome
+	                StringBuilder reversed = new StringBuilder(substring).reverse();
+	                
+	                if(substring.equals(reversed.toString())) {
+	                    // System.out.println("Added: " + substring); // Debugging line
+	                    allPalindromes.add(substring);
+	                }
+	            }
+	        }
+	        
+	        //System.out.println("--- Found Palindromes ---");
+	        // For BOBERRE, this list includes: B, O, B, E, R, R, E, RR, BOB
+	        
+	        // --- 2. Correctly Find the Longest Palindrome ---
+	        for(String p : allPalindromes) {
+	            // Compare the length of the current palindrome (p) with the current longest one
+	            if(p.length() > longestPalindrome.length()) {
+	                longestPalindrome = p; // Update the longest palindrome
+	            }
+	        }
+	        
+	        //System.out.println("--- Longest Palindrome ---");
+	        System.out.println(longestPalindrome);
+	     
 	      
 	           
 	          
